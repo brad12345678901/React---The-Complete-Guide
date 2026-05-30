87d86516514fe0814b89afa95f277b6c032dc5b1
Brad <ibanezbradley@gmail.com>
2026-05-30 17:47:02 +0800

    JS Refresher: Function within Function
    Demonstrate the ability to declare functions within functions

--- javascript-refresher-01/assets/scripts/app.js
+++ javascript-refresher-01/assets/scripts/app.js
@@ -1,19 +1,9 @@
-function handleTimeout () {
-    console.log("Timed Out!");
-}
+function init() {
+    function greet() {
+        console.log("Hi!");
+    }
 
-const handleTimeout2 =  () => {
-    console.log("Timed Out...again !");
-}
-
-setTimeout(handleTimeout, 2000);
-setTimeout(handleTimeout2, 3000);
-setTimeout(() => {
-    console.log("Timeout once more...!");
-}, 4000);
-
-function greeter(greet) {
     greet();
 }
 
-greeter(() => console.log("Hi!"))
+init()
