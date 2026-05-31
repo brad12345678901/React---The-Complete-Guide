e68f6073c9dcc6f5d0736257444d9dd5549c991e
Brad <ibanezbradley@gmail.com>
2026-05-31 16:53:48 +0800

    React Essentials: Alternative Approach to Conditional Rendering 2
    You can use normal variables to store HTML markup codes and use that to import into your component's HTML markup

--- react_app_sandbox/src/App.jsx
+++ react_app_sandbox/src/App.jsx
@@ -10,7 +10,20 @@ function App() {
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
   }
-  console.log("APP COMPONENT EXECUTING");
+  let tabContent = <p>Please Select a Topic</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <header>
@@ -42,17 +55,8 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {!selectedTopic && <p>Please Select a Topic</p>}
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
         </section>
+        {tabContent}
       </main>
     </div>
   );
