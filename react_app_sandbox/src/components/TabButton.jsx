976f2cb95a22b126a06fcd68c3b55e7bc6291504
Brad <ibanezbradley@gmail.com>
2026-05-31 19:16:35 +0800

    React Essentials: Forwarding Props to Wrapped Elements
    Utilized the Spread operator to map out the props passed in the component and spread them as attributes to your built-in elements to make it more flexible

--- react_app_sandbox/src/components/TabButton.jsx
+++ react_app_sandbox/src/components/TabButton.jsx
@@ -1,8 +1,7 @@
-export default function TabButton({ children, onSelect, isSelected }) {
-  console.log("TAB BUTTON COMPONENT EXECUTING");
+export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+      <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
     </li>
   );
 }
