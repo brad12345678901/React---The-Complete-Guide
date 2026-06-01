692367f6685cc262cf73e01c618d330a0a8d7a4c
Brad <ibanezbradley@gmail.com>
2026-06-01 20:21:03 +0800

    React Essentials: Implement Functional Approach for Setters of States
    To ensure data modification consistency, If you do a !state approach, this takes up the same old state value and apply that into your state to be updated if you called it two times or four times in one code block. This will look like a one time used button.
    
    Functional Approach like this one allows for it to pass the current state and then take the return value then set the state with that value on a queue like system or recursion

--- tic-tac-toe-project/components/Player.jsx
+++ tic-tac-toe-project/components/Player.jsx
@@ -4,13 +4,13 @@ export default function Player({name, symbol}) {
     const [isEditing, setIsEditing ] = useState(false);
 
     function handleEdit () {
-        setIsEditing(!isEditing);
+        setIsEditing(prev => !prev);
     }
 
     let playerName = <span className="player-name">{name}</span>;
 
     if (isEditing) {
-        playerName = <input type="text"></input>;
+        playerName = <input type="text" required value={name}></input>;
 
     }
   return (
