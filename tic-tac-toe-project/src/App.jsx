2457e4efbdf629ab54d98866be7b1b34b288ae6b
Brad <ibanezbradley@gmail.com>
2026-06-01 19:01:54 +0800

    React Essentials: Added Required Elements
    Progressing through a tic-tac-toe project by adding player identifiers in the container

--- tic-tac-toe-project/src/App.jsx
+++ tic-tac-toe-project/src/App.jsx
@@ -3,8 +3,22 @@ function App() {
   
 
   return (
-    <header>React Tic-Tac-Toe</header>
-  )
+    <main>
+      <div id = "game-container">
+        <ol id = "players">
+          <li>
+            <span className ="player-name">Player 1</span>
+            <span className ="player-name">X</span>
+          </li>
+          <li>
+            <span className ="player-name">Player 1</span>
+            <span className ="player-name">X</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+    </main>
+  );
 }
 
 export default App
