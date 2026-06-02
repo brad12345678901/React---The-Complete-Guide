02303ce465d8e009bae3fb39cd93d576d27584ac
Brad <ibanezbradley@gmail.com>
2026-06-03 02:31:50 +0800

    Investment Calculator Project: Implement Built Components
    Utilize the Reusable component with sample data

--- investment-calculator-project/src/App.jsx
+++ investment-calculator-project/src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Table from "./components/TableForm";
+import UserInputForm from "./components/UserInputForm";
+import { DATA_HEADER, DATA_SAMPLE } from "./SAMPLEDATA";
+import { calculateInvestmentResults } from "./util/investment";
+
+const userInvestmentInputData = {
+  initialInvestment: 0,
+  annualInvestment: 0,
+  expectedReturn: 0,
+  duration: 0,
+};
+
 function App() {
+  const [investmentData, setInvestmentData] = useState({
+    ...userInvestmentInputData,
+  });
+
+
+  const sampleData = calculateInvestmentResults({
+    initialInvestment: 15000,
+    annualInvestment: 9000,
+    expectedReturn: 5.5,
+    duration:2
+  });
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInputForm editInvestment={setInvestmentData} />
+      <Table headers={DATA_HEADER} data={DATA_SAMPLE} />
+    </>
+  );
 }
 
-export default App
+export default App;
