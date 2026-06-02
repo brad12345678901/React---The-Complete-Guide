be9c202074feea1f10355e197d02fd7bd6f94f78
Brad <ibanezbradley@gmail.com>
2026-06-02 21:48:40 +0800

    React Essentials: Refactor States to a Derived based structure, Add Winner Condition
    To effectively reduce the use of states, we extract the gameboard from the component gameboard and initialized the board over there and just pass the empty board into a prop of GameBoard.
    
    We also added an Winning condition to detect if a certain symbol completed a combination displaying a message of symbol that won. Utilizing the Function recall upon state update to use vanilla JS variables instead of states to check winning conditions. We derived from gameTurns state which holds information of the placements of symbols that we need

--- tic-tac-toe-project/src/App.jsx
+++ tic-tac-toe-project/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from "./components/Player.jsx";
-import GameBoard from "./components/GameBoard.jsx"
-import { useState } from "react"
+import GameBoard from "./components/GameBoard.jsx";
+import { useState } from "react";
 import Log from "./components/Log.jsx";
 import { WINNING_COMBINATIONS } from "./WINNING_COMBINATIONS.js";
 
-function derivedActivePlayer (gameTurns) {
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function derivedActivePlayer(gameTurns) {
   let currentPlayer = "X";
 
   if (gameTurns.length > 0 && gameTurns[0].player === "X") {
@@ -18,13 +24,42 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
   // const [activePlayer, setActivePlayer] = useState("X");
 
+  let gameBoard = initialGameBoard;
+
+  let winner;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
   const activePlayer = derivedActivePlayer(gameTurns);
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X");
-    setGameTurns(prevTurns => {
+    setGameTurns((prevTurns) => {
       const currentPlayer = derivedActivePlayer(prevTurns);
-      const updatedTurns = [{square: {row:rowIndex, col:colIndex}, player: activePlayer}, 
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
         ...prevTurns,
       ];
       return updatedTurns;
@@ -34,12 +69,25 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        {winner && <p>You Won, {winner}</p>}
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
+          board={gameBoard}
+        />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   );
 }
