16eb0cceab4198d6723f937b0bdc366b692f77bc
Brad <ibanezbradley@gmail.com>
2026-06-04 18:14:08 +0800

    Investment Calculator Project: Added Sample Data
    To benchmark the table form component before dealing with actual data

--- investment-calculator-project/src/SAMPLEDATA.js
+++ investment-calculator-project/src/SAMPLEDATA.js
@@ -1,23 +1,17 @@
-
 export const DATA_HEADER = [
-    {label: "Year", format: "text"},
-    {label: "Investment Value", format: "currency"},
-    {label: "Interest (Year)", format: "currency"},
-    {label: "Total Interest", format: "currency"},
-    {label: "Invested Capital", format: "currency"},
+  { label: "Year", format: "text" },
+  { label: "Investment Value", format: "currency" },
+  { label: "Interest (Year)", format: "currency" },
+  { label: "Total Interest", format: "currency" },
+  { label: "Invested Capital", format: "currency" },
 ];
 
-export const DATA_SAMPLE = [{
+export const DATA_SAMPLE = [
+  {
     year: 1,
-    investment_value:25,
-    interest: 25,
+    investment_value: 25,
+    annualInvestment: 25,
     total_interest: 25,
     invested_capital: 25,
-},
-{
-    year: 2,
-    investment_value:25,
-    interest: 25,
-    total_interest: 25,
-    invested_capital: 25,
-}];
+  },
+];
