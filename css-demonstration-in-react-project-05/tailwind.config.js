a90b35ada7621a4db6d89b6da15a66f7664c99bf
Brad <ibanezbradley@gmail.com>
2026-06-20 21:02:42 +0800

    Configurations for Tailwind CSS, extended the font family to have the Pacifico Font, otherwise defaults to cursive

--- css-demonstration-in-react-project-05/tailwind.config.js
+++ css-demonstration-in-react-project-05/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  ccontent: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['"Pacifico"', 'cursive']
+      }
+    },
   },
   plugins: [],
-};
+}
