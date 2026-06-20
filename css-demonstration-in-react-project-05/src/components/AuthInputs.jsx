027ef877879060e5d59207f876d72f410d533d7d
Brad <ibanezbradley@gmail.com>
2026-06-20 19:48:10 +0800

    Styling with React: Demonstrate conditional Rendering using Styled Components

--- css-demonstration-in-react-project-05/src/components/AuthInputs.jsx
+++ css-demonstration-in-react-project-05/src/components/AuthInputs.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { styled } from "styled-components";
 
-const ControlContainer = styled.p`
+const ControlContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
@@ -16,16 +16,17 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${(props) => props.invalid ? '#f87171' : '#6b7280'} ;
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
+  background-color: ${({invalid}) => invalid ? '#fed2d2' : '#d1d5db'};
+  color: ${({invalid}) => invalid ? '#ef4444' : '#374151'};
   border: 1px solid transparent;
+  border-color: ${({invalid}) => invalid ? '#f73f3f' : ''};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -54,25 +55,22 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p className="paragraph">
-          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
+          <Label invalid={emailNotValid}>
             Email
           </Label>
           <Input
             type="email"
-            // style= {{
-            //   backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db",
-            // }}
-            className={emailNotValid && "invalid"}
+            invalid={passwordNotValid}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
+          <Label invalid={emailNotValid}>
             Password
           </Label>
           <Input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
