abf2fcdf52d7e4dc6d2772abc1843254d7f05186
Brad <ibanezbradley@gmail.com>
2026-06-20 20:02:51 +0800

    Styling with React: Add Styled Component for Button

--- css-demonstration-in-react-project-05/src/components/AuthInputs.jsx
+++ css-demonstration-in-react-project-05/src/components/AuthInputs.jsx
@@ -16,20 +16,37 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${(props) => props.$invalid ? '#f87171' : '#6b7280'} ;
+  color: ${(props) => (props.$invalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
+  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
   border: 1px solid transparent;
-  border-color: ${({$invalid}) => $invalid ? '#f73f3f' : ''};
+  border-color: ${({ $invalid }) => ($invalid ? "#f73f3f" : "")};
   border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+  box-shadow:
+    0 1px 3px 0 rgba(0, 0, 0, 0.1),
+    0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -55,9 +72,7 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p className="paragraph">
-          <Label $invalid={emailNotValid}>
-            Email
-          </Label>
+          <Label $invalid={emailNotValid}>Email</Label>
           <Input
             type="email"
             $invalid={passwordNotValid}
@@ -65,9 +80,7 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <Label $invalid={emailNotValid}>
-            Password
-          </Label>
+          <Label $invalid={emailNotValid}>Password</Label>
           <Input
             type="password"
             $invalid={passwordNotValid}
@@ -81,9 +94,9 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <Button onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
