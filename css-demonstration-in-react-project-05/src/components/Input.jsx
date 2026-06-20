7ef72b35b74d5b63fdf1da17e2849669ee0a5e77
Brad <ibanezbradley@gmail.com>
2026-06-20 21:18:53 +0800

    Styling with React: Apply Conditional styles with Tailwind CSS

--- css-demonstration-in-react-project-05/src/components/Input.jsx
+++ css-demonstration-in-react-project-05/src/components/Input.jsx
@@ -1,7 +1,13 @@
 
 export default function Input({label, invalid, ...props}) {
+  let labelClass = "block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300"
+
+  if (invalid) {
+    labelClass = "block mb-2 text-xs font-bold tracking-wide uppercase text-red-400"
+  }
+
     return <p>
-        <label className="block mb-2 text-xs font-bold tracking-wide uppercase text-stone-300">{label}</label>
+        <label className={labelClass}>{label}</label>
         <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow bg-stone-300"{...props} />
     </p>
 }
