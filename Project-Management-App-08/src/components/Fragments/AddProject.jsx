2385c0312c9f37af837e3d0b87874923f5963a88
Brad <ibanezbradley@gmail.com>
2026-06-23 21:04:14 +0800

    Project Management App: Put a submit handler to extract data from inputs

--- Project-Management-App-08/src/components/Fragments/AddProject.jsx
+++ Project-Management-App-08/src/components/Fragments/AddProject.jsx
@@ -1,18 +1,30 @@
 import Button from "../Button";
 import Input from "../forms/Input";
 
-export default function AddProject({ onSubmit, ...props }) {
+export default function AddProject({ onSubmit, setActivity, ...props }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+    setActivity("home");
+  }
+
   return (
-    <div className="w-full grid gap-2">
+    <form className="w-full grid gap-2" onSubmit={handleSubmit}>
       <span className="flex gap-5 justify-end">
         <Button plain className="hover:text-slate-500">
           Cancel
         </Button>
-        <Button>Save</Button>
+        <Button type="submit">Save</Button>
       </span>
-      <Input type="text" placeholder="Enter Project Name" />
-      <Input type="textarea" placeholder="Enter Project Description" />
-      <Input type="date" />
-    </div>
+      <Input type="text" name="projectName" placeholder="Enter Project Name" />
+      <Input
+        type="textarea"
+        name="projectDescription"
+        placeholder="Enter Project Description"
+      />
+      <Input type="date" name="projectDate" />
+    </form>
   );
 }
