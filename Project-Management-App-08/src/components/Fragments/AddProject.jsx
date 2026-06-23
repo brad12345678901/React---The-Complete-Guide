56614c9c7b2eb4d0cfb95628b5d5fbf57c1b3b74
Brad <ibanezbradley@gmail.com>
2026-06-23 20:48:59 +0800

    Project Management App: Developed Add Project Component

--- Project-Management-App-08/src/components/Fragments/AddProject.jsx
+++ Project-Management-App-08/src/components/Fragments/AddProject.jsx
@@ -1,5 +1,18 @@
-export default function AddProject() {
-    return <div>
-        TEST
+import Button from "../Button";
+import Input from "../forms/Input";
+
+export default function AddProject({ onSubmit, ...props }) {
+  return (
+    <div className="w-full grid gap-2">
+      <span className="flex gap-5 justify-end">
+        <Button plain className="hover:text-slate-500">
+          Cancel
+        </Button>
+        <Button>Save</Button>
+      </span>
+      <Input type="text" placeholder="Enter Project Name" />
+      <Input type="textarea" placeholder="Enter Project Description" />
+      <Input type="date" />
     </div>
+  );
 }
