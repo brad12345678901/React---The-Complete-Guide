64b134cd52eeb76d1af87d63eb4d479564021ae0
Brad <ibanezbradley@gmail.com>
2026-06-24 16:01:54 +0800

    Project Management App: Replace nav element with an unordered list, prettified code indentations

--- Project-Management-App-08/src/components/Fragments/ProjectNavigationPane.jsx
+++ Project-Management-App-08/src/components/Fragments/ProjectNavigationPane.jsx
@@ -1,23 +1,33 @@
 import { useRef } from "react";
 import classes from "../../assets/projectnavigation.module.css";
 
-export default function ProjectNavigationPane({ listofProjects, selectedProjectRef, setActivity, ...props }) {
+export default function ProjectNavigationPane({
+  listofProjects,
+  selectedProjectRef,
+  setActivity,
+  ...props
+}) {
   if (!listofProjects) {
     return null;
   }
 
-  function handleSelectProject (e) {
+  function handleSelectProject(e) {
     setActivity(`viewproject:${e.target.id}`);
     selectedProjectRef.current = e.target;
   }
 
   return (
-    <nav className={`py-8 overflow-x-hidden max-h-[500px] ${classes.navpane}`}>
+    <ul className={`py-8 overflow-x-hidden max-h-[500px] ${classes.navpane}`}>
       {listofProjects.map((entry, index) => (
-        <li onClick={handleSelectProject} key={`list:${index}`} id={index} className="text-white list-none cursor-pointer text-nowrap py-2">
+        <li
+          onClick={handleSelectProject}
+          key={`list:${index}`}
+          id={index}
+          className="text-white list-none cursor-pointer text-nowrap py-2 hover:bg-gray-800 rounded-md px-2"
+        >
           {entry.projectName}
         </li>
       ))}
-    </nav>
+    </ul>
   );
 }
