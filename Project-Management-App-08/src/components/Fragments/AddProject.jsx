d4b45c3c8d080200c7e31d812d5fa4649c02d8df
Brad <ibanezbradley@gmail.com>
2026-06-24 22:44:22 +0800

    PMA: Added error input validations, added a guard check before adding the project

--- Project-Management-App-08/src/components/Fragments/AddProject.jsx
+++ Project-Management-App-08/src/components/Fragments/AddProject.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../Button";
 import Input from "../forms/Input";
 
@@ -7,18 +8,43 @@ export default function AddProject({
   setListofProject,
   ...props
 }) {
+  const [errors, setErrors] = useState({
+    projectName: undefined,
+    projectDescription: undefined,
+    projectDate: undefined,
+  });
+
   function handleSubmit(e) {
+    let error = false;
     e.preventDefault();
     const formData = new FormData(e.target);
     formData.append("tasks", "");
     const data = Object.fromEntries(formData.entries());
-    setListofProject(prev => [...prev, data]);
-    setActivity("home");
+    console.log(data);
+
+    for (const [key, value] of Object.entries(data)) {
+      if (key != "tasks") {
+        if (!value) {
+          error = true;
+          setErrors((prev) => {
+            return {
+              ...prev,
+              [key]: `Should not be empty`,
+            };
+          });
+        }
+      }
+    }
+
+    if (!error) {
+      setListofProject((prev) => [...prev, data]);
+      setActivity("home");
+    }
   }
 
   return (
-    <form className="w-full grid gap-2" onSubmit={handleSubmit}>
-      <span className="flex gap-5 justify-end">
+    <form className="w-full space-y-6" onSubmit={handleSubmit}>
+      <div className="block space-x-4 text-right">
         <Button
           type="button"
           plain
@@ -28,14 +54,20 @@ export default function AddProject({
           Cancel
         </Button>
         <Button type="submit">Save</Button>
-      </span>
-      <Input type="text" name="projectName" placeholder="Enter Project Name" />
+      </div>
+      <Input
+        type="text"
+        name="projectName"
+        placeholder="Enter Project Name"
+        errors={errors.projectName}
+      />
       <Input
         type="textarea"
         name="projectDescription"
         placeholder="Enter Project Description"
+        errors={errors.projectDescription}
       />
-      <Input type="date" name="projectDate" />
+      <Input type="date" name="projectDate" errors={errors.projectDate} />
     </form>
   );
 }
--- Project-Management-App-08/src/components/forms/Input.jsx
+++ Project-Management-App-08/src/components/forms/Input.jsx
@@ -1,18 +1,24 @@
-export default function Input({ type, className, ...props }) {
+export default function Input({ type, className, errors, ...props }) {
   if (type === "textarea") {
     return (
-      <textarea
-        {...props}
-        className={`bg-zinc-200 p-2 outline-none focus:border-b-2 focus:border-b-slate-400 rounded-md ${className}`}
-      ></textarea>
+      <div>
+        <textarea
+          {...props}
+          className={`w-full bg-zinc-200 p-2 outline-none ${errors ? "border border-red-500 focus:border-red-600" : "focus:border-b-2 focus:border-b-slate-400"} rounded-md min-h-28  ${className}`}
+        />
+        {errors && <span className="block text-xs text-red-500">{errors}</span>}
+      </div>
     );
   }
 
   return (
-    <input
-      type={type}
-      {...props}
-      className={`bg-zinc-200 p-2 outline-none focus:border-b-2 focus:border-b-slate-400 rounded-md max-h-10 ${className}`}
-    ></input>
+    <div>
+      <input
+        type={type}
+        {...props}
+        className={`w-full bg-zinc-200 p-2 outline-none ${errors ? "border border-red-500 focus:border-red-600" : "focus:border-b-2 focus:border-b-slate-400"} rounded-md max-h-10 ${className}`}
+      />
+      {errors && <span className="block text-xs text-red-500">{errors}</span>}
+    </div>
   );
 }
