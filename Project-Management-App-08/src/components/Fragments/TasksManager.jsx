4ef391444d473b5305117d0406f8a62eee682ddc
Brad <ibanezbradley@gmail.com>
2026-06-24 15:41:27 +0800

    Project Management App: Update Task Manager to Handle Empty Tasks

--- Project-Management-App-08/src/components/Fragments/TasksManager.jsx
+++ Project-Management-App-08/src/components/Fragments/TasksManager.jsx
@@ -7,6 +7,11 @@ export default function TasksManager({
   setListOfProject,
   ...props
 }) {
+    
+  function handleAddTask(e) {
+    e.preventDefault();
+  }
+
   function handleClear(e) {
     const filteredTask = tasks.filter((_, index) => index != e.target.id);
     setListOfProject((prevList) =>
@@ -25,35 +30,33 @@ export default function TasksManager({
   return (
     <div>
       <h1 className="py-5 text-3xl font-semibold">Tasks</h1>
-      <span className="flex items-center gap-2 pb-6">
+      <form className="flex items-center gap-2 pb-6" onSubmit={handleAddTask}>
         <Input type="text" className="my-2 w-2/6" />
-        <Button plain className="hover:text-slate-500">
+        <Button type="submit" plain className="hover:text-slate-500">
           Add Task
         </Button>
-      </span>
+      </form>
       <ul className="flex flex-col gap-2 bg-gray-200 list-none py-6 px-2 rounded-md">
-        <li className="flex items-center justify-between">
-          <span>TEST LIST</span>
-          <Button plain className="hover:text-red-600">
-            Clear
-          </Button>
-        </li>
-        {tasks.map((task, index) => (
-          <li
-            key={`tasks:${index}`}
-            className="flex items-center justify-between"
-          >
-            <span>{task}</span>
-            <Button
-              onClick={handleClear}
-              id={index}
-              plain
-              className="hover:text-red-600"
+        {Array.isArray(tasks) ? (
+          tasks.map((task, index) => (
+            <li
+              key={`tasks:${index}`}
+              className="flex items-center justify-between"
             >
-              Clear
-            </Button>
-          </li>
-        ))}
+              <span>{task}</span>
+              <Button
+                onClick={handleClear}
+                id={index}
+                plain
+                className="hover:text-red-600"
+              >
+                Clear
+              </Button>
+            </li>
+          ))
+        ) : (
+          <p className="place-self-center">No Tasks Available</p>
+        )}
       </ul>
     </div>
   );
