fdd1029d71552d1e0e994f22faef0f57bdfa8ad9
Brad <ibanezbradley@gmail.com>
2026-06-24 23:11:21 +0800

    PMA: Fixed unresetting error for input tasks

--- Project-Management-App-08/src/components/Fragments/TasksManager.jsx
+++ Project-Management-App-08/src/components/Fragments/TasksManager.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Button from "../Button";
 import Input from "../forms/Input";
 
@@ -7,25 +8,34 @@ export default function TasksManager({
   setListOfProject,
   ...props
 }) {
+  const [error, setError] = useState(undefined);
+  useEffect(() => {
+    setError(undefined);
+  }, [projectIndex]);
   function handleAddTask(e) {
     e.preventDefault();
-
+    let error = false;
     const formData = new FormData(e.target);
     const tempTasks = [...tasks];
+
     for (const i of formData.values()) {
-      tempTasks.push(i);
+      if (!i) {
+        error = true;
+        setError("Task cannot be empty");
+      } else tempTasks.push(i);
     }
-    setListOfProject((prevList) =>
-      prevList.map((project, index) => {
-        if (index == projectIndex) {
-          return {
-            ...project,
-            tasks: tempTasks,
-          };
-        }
-        return project;
-      }),
-    );
+    if (error)
+      setListOfProject((prevList) =>
+        prevList.map((project, index) => {
+          if (index == projectIndex) {
+            return {
+              ...project,
+              tasks: tempTasks,
+            };
+          }
+          return project;
+        }),
+      );
   }
 
   function handleClear(e) {
@@ -47,7 +57,7 @@ export default function TasksManager({
     <div>
       <h1 className="py-5 text-3xl font-semibold">Tasks</h1>
       <form className="flex items-center gap-2 pb-6" onSubmit={handleAddTask}>
-        <Input type="text" name="tasks" className="my-2 w-2/6" />
+        <Input type="text" name="tasks" className="my-2 w-2/6" errors={error} />
         <Button type="submit" plain className="hover:text-slate-500">
           Add Task
         </Button>
@@ -71,7 +81,9 @@ export default function TasksManager({
             </li>
           ))
         ) : (
-          <p className="place-self-center text-gray-500 font-semibold">This project doesn't have any tasks</p>
+          <p className="place-self-center text-gray-500 font-semibold">
+            This project doesn't have any tasks
+          </p>
         )}
       </ul>
     </div>
