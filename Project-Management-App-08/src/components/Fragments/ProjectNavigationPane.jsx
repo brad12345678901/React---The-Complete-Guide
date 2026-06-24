83ed4715465662f17fa58a818dec9932e48d432b
Brad <ibanezbradley@gmail.com>
2026-06-24 15:23:18 +0800

    Project Management App: Add the ability to view projects

--- Project-Management-App-08/src/components/Fragments/ProjectNavigationPane.jsx
+++ Project-Management-App-08/src/components/Fragments/ProjectNavigationPane.jsx
@@ -1,13 +1,20 @@
+import { useRef } from "react";
 import classes from "../../assets/projectnavigation.module.css";
 
-export default function ProjectNavigationPane({ listofProjects, ...props }) {
+export default function ProjectNavigationPane({ listofProjects, selectedProjectRef, setActivity, ...props }) {
   if (!listofProjects) {
     return null;
   }
+
+  function handleSelectProject (e) {
+    setActivity(`viewproject:${e.target.id}`);
+    selectedProjectRef.current = e.target;
+  }
+
   return (
     <nav className={`py-8 overflow-x-hidden max-h-[500px] ${classes.navpane}`}>
       {listofProjects.map((entry, index) => (
-        <li className="text-white list-none cursor-pointer text-nowrap py-2">
+        <li onClick={handleSelectProject} key={`list:${index}`} id={index} className="text-white list-none cursor-pointer text-nowrap py-2">
           {entry.projectName}
         </li>
       ))}
