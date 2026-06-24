3a548862f7bdbf7736cf12324a7f1f4ec78a1759
Brad <ibanezbradley@gmail.com>
2026-06-24 23:10:35 +0800

    PMA: make span text binded to the input and prevent it from taking up space

--- Project-Management-App-08/src/components/forms/Input.jsx
+++ Project-Management-App-08/src/components/forms/Input.jsx
@@ -1,24 +1,24 @@
 export default function Input({ type, className, errors, ...props }) {
   if (type === "textarea") {
     return (
-      <div>
+      <div className="relative flex flex-col">
         <textarea
           {...props}
           className={`w-full bg-zinc-200 p-2 outline-none ${errors ? "border border-red-500 focus:border-red-600" : "focus:border-b-2 focus:border-b-slate-400"} rounded-md min-h-28  ${className}`}
         />
-        {errors && <span className="block text-xs text-red-500">{errors}</span>}
+        {errors && <span className="absolute -bottom-4 text-xs text-red-500">{errors}</span>}
       </div>
     );
   }
 
   return (
-    <div>
+    <div className="relative flex flex-col">
       <input
         type={type}
         {...props}
         className={`w-full bg-zinc-200 p-2 outline-none ${errors ? "border border-red-500 focus:border-red-600" : "focus:border-b-2 focus:border-b-slate-400"} rounded-md max-h-10 ${className}`}
       />
-      {errors && <span className="block text-xs text-red-500">{errors}</span>}
+      {errors && <span className="absolute -bottom-4 text-xs text-red-500">{errors}</span>}
     </div>
   );
 }
