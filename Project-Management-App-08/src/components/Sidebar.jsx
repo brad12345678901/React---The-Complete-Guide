0c4e90137ccba3b1452f09b65992820529d7e777
Brad <ibanezbradley@gmail.com>
2026-06-24 15:23:54 +0800

    Project Management App: Update Sidebar to pass setter function to child components

--- Project-Management-App-08/src/components/Sidebar.jsx
+++ Project-Management-App-08/src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import Button from "./Button";
 import ProjectNavigationPane from "./Fragments/ProjectNavigationPane";
 
-export default function Sidebar({className, setActivity, projectList, ...props}) {
+export default function Sidebar({className, setActivity, projectList, selectedProjectRef, ...props}) {
     return <div className = "p-6 bg-black w-1/3 rounded-r-lg">
         <h1 className = "text-white my-8 font-semibold text-2xl">Your Projects</h1>
         <Button onClick={() => {setActivity("addproject")}}>+ Add Project</Button>
-        <ProjectNavigationPane listofProjects={projectList}/>
+        <ProjectNavigationPane listofProjects={projectList} selectedProjectRef={selectedProjectRef} setActivity={setActivity}/>
     </div>
 }
