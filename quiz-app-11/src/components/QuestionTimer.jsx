18eb0c948746596dd03a6727570d0b870c3b3f11
Brad <ibanezbradley@gmail.com>
2026-07-02 20:57:20 +0800

    Quiz App: Fix the infinite recreation of Timeout using useCallback
    So the function to skip the answer is not recreated every rerender

--- quiz-app-11/src/components/QuestionTimer.jsx
+++ quiz-app-11/src/components/QuestionTimer.jsx
@@ -4,10 +4,12 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
+    console.log("TIMEOUT");
     setTimeout(onTimeout, timeout);
   }, [timeout, onTimeout]);
 
   useEffect(() => {
+    console.log("INTERVAL");
     setInterval(() => {
       setRemainingTime((prevTime) => prevTime - 100);
     }, 100);
