037756acbf42105017cfbd4620cd1fc26bc47836
Brad <ibanezbradley@gmail.com>
2026-07-02 20:20:45 +0800

    Quiz App: Place the shuffled answers after the boolean quiz is complete
    To avoid the array out of bounds error

--- quiz-app-11/src/components/Quiz.jsx
+++ quiz-app-11/src/components/Quiz.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions.js";
+import QuizComplete from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswer) => {
@@ -15,8 +15,20 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={QuizComplete} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
-    <div id = "quiz">
+    <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
