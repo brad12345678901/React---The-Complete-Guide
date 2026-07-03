f879cd1aa79a57478c71a497178bf9cd1fb78a72
Brad <ibanezbradley@gmail.com>
2026-07-03 22:21:44 +0800

    Quiz App: Create a summary component
    To showcase all the correct, skipped and wrong answers

--- quiz-app-11/src/components/Quiz.jsx
+++ quiz-app-11/src/components/Quiz.jsx
@@ -1,10 +1,11 @@
 import { useState, useCallback, useRef } from "react";
 
 import QUESTIONS from "../questions.js";
-import QuizComplete from "../assets/quiz-complete.png";
+
 import Answers from "./Answers.jsx";
 import QuestionTimer from "./QuestionTimer.jsx";
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -26,12 +27,7 @@ export default function Quiz() {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={QuizComplete} alt="Trophy icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
