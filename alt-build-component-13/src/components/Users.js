8e343523c2fbd259989c77a8d08c7d155073135d
Brad <ibanezbradley@gmail.com>
2026-07-05 22:35:16 +0800

    Alternative Components: Dealing with States and Events traditionally
    Before, states are only in one variable inside your class component
    
    all must be compacted into one in one state variable via constructor
    
    you can also do event handlers but the approach is different
    
    you have to bind the passed function handler to effectively not lose the link between the class and the function so it can access the variables within the class still.
    
    and you have to use the this keyword alot
    
    also you must call the super() function in the constructor when initializing

--- alt-build-component-13/src/components/Users.js
+++ alt-build-component-13/src/components/Users.js
@@ -1,37 +1,79 @@
-import { useState } from 'react';
-import User from './User';
+import { Component, useState } from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
+import classes from "./Users.module.css";
 
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+class Users extends Component {
+  // FUNCTION THAT EXECUTES UPON INSTANTIATE
+  // STATE MUST BE AN OBJECT
+  // AND STATE MUST BE STRICTLY STATE, NO CUSTOM NAMES
+  // AND ONLY ONE STATE THAT GROUPS ALL DATA TOGETHER
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+      moreState: "test",
+    };
+  }
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
+  //FUNCTION DECLARED WITHIN THE CLASS, NOT WITHIN THE RENDER FUNCTION
+  toggleUsersHandler() {
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
 
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+    console.log(usersList);
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? "Hide" : "Show"} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
 
 export default Users;
