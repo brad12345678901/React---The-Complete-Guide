7c555587ade3566ab32defa4e646a91ecd858118
Brad <ibanezbradley@gmail.com>
2026-07-06 20:45:38 +0800

    Http Request: Fallback Error
    Provide a fallback when the request is not ok by rendering an error component.

--- http-requests-14/src/components/AvailablePlaces.jsx
+++ http-requests-14/src/components/AvailablePlaces.jsx
@@ -1,21 +1,37 @@
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 import { useEffect, useState } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/placessssssss");
+        const resData = await response.json();
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (err) {
+        setError({
+          message:
+            err.message || "Could not fetch places, please try again later",
+        });
+      }
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occured!" message={error.message}></Error>;
+  }
 
   return (
     <Places
