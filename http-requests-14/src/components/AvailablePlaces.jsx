f068c6b0fa7b7a7cd0ea809d18b35cf2d5645720
Brad <ibanezbradley@gmail.com>
2026-07-10 20:01:34 +0800

    Custom Hooks: Bring back the sorting of geolocation
    Create a asynchronous function that returns a promise object, run the navigator within and resolve it via passing in the sorted places data
    
    Then we pass that function as a function to call in within the custom fetch hook

--- http-requests-14/src/components/AvailablePlaces.jsx
+++ http-requests-14/src/components/AvailablePlaces.jsx
@@ -1,28 +1,31 @@
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
-import { useEffect, useState } from "react";
 import { sortPlacesByDistance } from "../loc.js";
 import { fetchAvailablePlaces } from "../http.js";
 import { useFetch } from "../hooks/useFetch.js";
 
-// navigator.geolocation.getCurrentPosition((position) => {
-//   console.log(position);
-//   const sortedPlaces = sortPlacesByDistance(
-//     places,
-//     position.coords.latitude,
-//     position.coords.longitude,
-//   );
-//   setAvailablePlaces(sortedPlaces);
-//   setIsFetching(false);
-// });
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude,
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const {
     isFetching,
     error,
     fetchedData: availablePlaces,
-    setFetchedData: setAvailablePlaces,
-  } = useFetch(fetchAvailablePlaces, []);
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <Error title="An error occured!" message={error.message}></Error>;
