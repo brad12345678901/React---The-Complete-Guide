1cafa8891375c883c5086d050ab2f57bad2ab2a0
Brad <ibanezbradley@gmail.com>
2026-07-11 15:29:46 +0800

    Forms: Add a separate component fetching input values by Refs
    To demonstrate different approaches to input values

--- forms-15/src/App.jsx
+++ forms-15/src/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./components/Header.jsx";
 import Signup from "./components/Signup.jsx";
 import Login from "./components/Login.jsx";
+import LoginRefs from "./components/LoginRefs.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <LoginRefs />
       </main>
     </>
   );
