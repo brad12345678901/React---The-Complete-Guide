15f7da73a0851b4c889877fdc3cdd249595936e9
Brad <ibanezbradley@gmail.com>
2026-07-15 21:27:14 +0800

    Food App: Add a currency formatter utility

--- 18-food-order-app/src/components/MealItem.jsx
+++ 18-food-order-app/src/components/MealItem.jsx
@@ -1,11 +1,14 @@
+import { usdFormatter } from "../utils/formatter";
+
 export default function MealItem({ item }) {
+  
   return (
     <div className="meal-item">
       <article>
         <img src={`http://localhost:3000/${item.image}`} />
         <h3>{item.name}</h3>
         <div>
-          <p className="meal-item-price">{item.price}</p>
+          <p className="meal-item-price">{usdFormatter.format(item.price)}</p>
           <div className="meal-item-description">{item.description}</div>
         </div>
         <div className="meal-item-actions">
