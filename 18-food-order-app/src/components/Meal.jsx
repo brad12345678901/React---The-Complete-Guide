bb7b7f6949477eade60b3ee0af4df2dbbf1e8995
Brad <ibanezbradley@gmail.com>
2026-07-15 21:02:58 +0800

    Food App: Update Meal Component
    Wrap it in a article element

--- 18-food-order-app/src/components/Meal.jsx
+++ 18-food-order-app/src/components/Meal.jsx
@@ -1,12 +1,14 @@
 export default function Meal({ item }) {
   return (
     <div className="meal-item">
-      <img src={`http://localhost:3000/${item.image}`} />
-      <h3>{item.name}</h3>
-      <div className="meal-item-description">{item.description}</div>
-      <div className="meal-item-actions">
-        <button className="button">Add to Cart</button>
-      </div>
+      <article>
+        <img src={`http://localhost:3000/${item.image}`} />
+        <h3>{item.name}</h3>
+        <div className="meal-item-description">{item.description}</div>
+        <div className="meal-item-actions">
+          <button className="button">Add to Cart</button>
+        </div>
+      </article>
     </div>
   );
 }
