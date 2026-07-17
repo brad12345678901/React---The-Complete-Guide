0dc1c8878575a200dded382351e1161a3dbf1015
Brad <ibanezbradley@gmail.com>
2026-07-17 17:25:17 +0800

    Food App: Add total Pricing

--- 18-food-order-app/src/components/Cart.jsx
+++ 18-food-order-app/src/components/Cart.jsx
@@ -2,9 +2,16 @@ import { useContext } from "react";
 import { StoreContext } from "../store/storeContext";
 import CartItem from "./CartItem";
 import Button from "./Button";
+import { usdFormatter } from "../utils/formatter";
 
 export default function Cart({ close }) {
   const { state } = useContext(StoreContext);
+
+  const total = state.mealItems.reduce(
+    (result, item) => result + item.price * item.quantity,
+    0,
+  );
+
   return (
     <div className="cart">
       <h2>Your Cart</h2>
@@ -15,8 +22,11 @@ export default function Cart({ close }) {
           state.mealItems.map((item) => <CartItem key={item.id} item={item} />)
         )}
       </ul>
+      <p className="cart-total">{usdFormatter.format(total)}</p>
       <div className="modal-actions">
-        <Button onClick={close} textButton>Close</Button>
+        <Button onClick={close} textButton>
+          Close
+        </Button>
         <Button onClick={close}>Checkout</Button>
       </div>
     </div>
