84e8d7c98637e38f383d1109c3732e8ec0448cbd
Brad <ibanezbradley@gmail.com>
2026-07-17 17:00:31 +0800

    Food App: Integrate add/remove meal item into the cart modal

--- 18-food-order-app/src/components/CartItem.jsx
+++ 18-food-order-app/src/components/CartItem.jsx
@@ -1,11 +1,15 @@
-export default function CartItem ({ item }) {
+import { useContext } from "react";
+import { StoreContext } from "../store/storeContext";
+
+export default function CartItem({ item }) {
+  const { addMealItem, removeMealItem } = useContext(StoreContext);
   return (
     <li className="cart-item">
       <p>{item.name}</p>
       <span className="cart-item-actions">
-        <button>-</button>
+        <button onClick={() => removeMealItem(item)}>-</button>
         <p>{item.quantity}</p>
-        <button>+</button>
+        <button onClick={() => addMealItem(item)}>+</button>
       </span>
     </li>
   );
