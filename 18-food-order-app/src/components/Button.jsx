39b62ffcc2884212af259e4ee1de127ef39d3362
Brad <ibanezbradley@gmail.com>
2026-07-19 19:10:13 +0800

    Food App: expand button component to include form status

--- 18-food-order-app/src/components/Button.jsx
+++ 18-food-order-app/src/components/Button.jsx
@@ -1,8 +1,23 @@
-export default function Button({ children, textButton, ...props }) {
+import { useFormStatus } from "react-dom";
+
+export default function Button({
+  children,
+  textButton,
+  fallbackText,
+  type,
+  ...props
+}) {
   const className = !textButton ? "button" : "text-button";
+  const isSubmitButton = type === "submit" ? true : false;
+  const { pending } = useFormStatus();
   return (
-    <button className={className} {...props}>
-      {children}
+    <button
+      className={className}
+      type={type}
+      {...props}
+      disabled={isSubmitButton ? pending : false}
+    >
+      {pending ? fallbackText : children}
     </button>
   );
 }
