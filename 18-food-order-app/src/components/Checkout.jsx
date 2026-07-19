98e30c3c54619d51035a680a564e09085273778d
Brad <ibanezbradley@gmail.com>
2026-07-19 18:27:09 +0800

    Food App: built checkout modal interface

--- 18-food-order-app/src/components/Checkout.jsx
+++ 18-food-order-app/src/components/Checkout.jsx
@@ -1,17 +1,38 @@
 import { useContext } from "react";
 import Button from "./Button";
 import { StoreContext } from "../store/storeContext";
+import Input from "./Input";
+import { usdFormatter } from "../utils/formatter";
 
-export default function Checkout({close}) {
+export default function Checkout({ close }) {
+  const { state } = useContext(StoreContext);
+  const total = state.mealItems.reduce(
+    (result, item) => result + item.price * item.quantity,
+    0,
+  );
   return (
     <div>
       <h2>Checkout</h2>
+      <p>Total Amount: {usdFormatter.format(total)}</p>
       <form>
-
+        <Input label="Full Name" id="full-name" />
+        <Input
+          label="Email Address"
+          id="email"
+          type="email"
+          autoComplete="off"
+        />
+        <Input label="Street" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" id="postal-code" autoComplete="off" />
+          <Input label="City" id="city" />
+        </div>
       </form>
       <div className="modal-actions">
-        <Button onClick={close} textButton>Close</Button>
-        <Button>Submit</Button>
+        <Button onClick={close} textButton>
+          Close
+        </Button>
+        <Button type="submit">Submit</Button>
       </div>
     </div>
   );
