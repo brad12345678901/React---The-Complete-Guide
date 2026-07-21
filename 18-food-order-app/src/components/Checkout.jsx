9989116f01b2032e8d9e0b88390f8306ebae2fde
Brad <ibanezbradley@gmail.com>
2026-07-21 16:49:43 +0800

    Food App: Implement input validators

--- 18-food-order-app/src/components/Checkout.jsx
+++ 18-food-order-app/src/components/Checkout.jsx
@@ -5,7 +5,7 @@ import Input from "./Input";
 import { usdFormatter } from "../utils/formatter";
 import { useActionState } from "react";
 import { testFunction } from "../utils/api";
-import { hasAlphabets } from "../utils/validator";
+import { hasAlphabets, isEmail, isEmpty } from "../utils/validator";
 
 export default function Checkout({ close, controller }) {
   const { state, dispatch } = useContext(StoreContext);
@@ -21,13 +21,50 @@ export default function Checkout({ close, controller }) {
     const postalCode = formData.get("postal-code");
     const city = formData.get("city");
 
+    const errors = {};
+
+    if (!isEmail(email) && isEmpty(email)) {
+      errors.email = "Not a valid email";
+    }
+
+    if (isEmpty(fullName)) {
+      errors.fullName = "Full name should not be empty";
+    }
+
+    if (isEmpty(street)) {
+      errors.street = "Street should not be empty";
+    }
+
+    if (isEmpty(city)) {
+      errors.city = "City should not be empty";
+    }
+
+    if (hasAlphabets(postalCode) || isEmpty(postalCode)) {
+      errors.postalCode = "It must be number only / Not empty";
+    }
+
+    console.log(errors);
+
+    if (Object.keys(errors).length) {
+      return {
+        defaultValues: {
+          fullName,
+          email,
+          street,
+          postalCode,
+          city,
+        },
+        errors: errors,
+      };
+    }
+
     const result = await testFunction(controller.current.signal);
 
-    if (result.success)
-      dispatch({ type: "PROGRESS_MODAL" });
+    if (result.success) dispatch({ type: "PROGRESS_MODAL" });
   }
 
   const [formState, formAction] = useActionState(handleAction, {
+    defaultValues: {},
     errors: null,
   });
 
@@ -36,23 +73,45 @@ export default function Checkout({ close, controller }) {
       <h2>Checkout</h2>
       <p>Total Amount: {usdFormatter.format(total)}</p>
       <form action={formAction}>
-        <Input label="Full Name" id="full-name" name="full-name" />
+        <Input
+          label="Full Name"
+          id="full-name"
+          name="full-name"
+          error={formState.errors?.fullName}
+          defaultValue={formState.defaultValues?.fullName}
+        />
         <Input
           label="Email Address"
           id="email"
           name="email"
           type="email"
           autoComplete="off"
+          error={formState.errors?.email}
+          defaultValue={formState.defaultValues?.email}
         />
-        <Input label="Street" id="street" name="street" />
+        <Input
+          label="Street"
+          id="street"
+          name="street"
+          error={formState.errors?.street}
+          defaultValue={formState.defaultValues?.street}
+        />
         <div className="control-row">
           <Input
             label="Postal Code"
             id="postal-code"
             name="postal-code"
             autoComplete="off"
+            error={formState.errors?.postalCode}
+            defaultValue={formState.defaultValues?.postalCode}
           />
-          <Input label="City" id="city" name="city" />
+          <Input
+            label="City"
+            id="city"
+            name="city"
+            error={formState.errors?.city}
+            defaultValue={formState.defaultValues?.city}
+          />
         </div>
         <div className="modal-actions">
           <Button onClick={close} type="button" textButton fallbackText="Close">
--- 18-food-order-app/src/components/Input.jsx
+++ 18-food-order-app/src/components/Input.jsx
@@ -1,8 +1,9 @@
-export default function Input({ label, id, ...props }) {
+export default function Input({ label, id, error, ...props }) {
   return (
     <span className="control">
       <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} {...props}/>
+      <input type="text" id={id} {...props} />
+      {error && <p className="error-paragraph">{error}</p>}
     </span>
   );
 }
